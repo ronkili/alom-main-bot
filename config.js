87daaa6dcd97b3e8f1e9788ddc6ceb52a8aeda6d
ronkili <ronaa0269@gmail.com>
2026-09-23 13:08:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,6 @@ module.exports = {
   // בנוסף, הבוט מזהה אוטומטית שמות כמו:
   // updates / announcements / news / rules / עדכונים / חוקים
   verifyReadOnlyChannelIds: [
-    verifyReadOnlyChannelIds: [
   "1552250283843915828",
   "1552247120122216548",
   "1552249838001856522",
@@ -25,8 +24,10 @@ module.exports = {
   "1552241917302013972",
   "1552243027987275786",
   "1552243078973104128"
+    "1552258436526247978"
+"1552258486564556801"
+"1552258615421960215"
 ],
-  ],
 
   // =====================
   // STAFF
@@ -34,34 +35,34 @@ module.exports = {
 
   // Staff role used by:
   // !rank, !h claims, moderation and ticket controls.
-  staffRoleId: "PUT_STAFF_ROLE_ID_HERE",
+  staffRoleId: "1552259334078074932",
 
   // =====================
   // MODERATION
   // =====================
 
   // Role used for Chat Mute.
-  muteRoleId: "PUT_MUTE_ROLE_ID_HERE",
+  muteRoleId: "1552259390936190996",
 
   // Moderation logs channel.
   // Leave "" if you do not want mod logs.
-  modLogsChannelId: "PUT_MOD_LOGS_CHANNEL_ID_HERE",
+  modLogsChannelId: "1552259532539830323",
 
   // =====================
   // TICKETS
   // =====================
 
   // Category where tickets are created.
-  ticketCategoryId: "PUT_TICKET_CATEGORY_ID_HERE",
+  ticketCategoryId: "1552242972119277628",
 
   // Staff role for normal tickets.
-  ticketStaffRoleId: "PUT_TICKET_STAFF_ROLE_ID_HERE",
+  ticketStaffRoleId: "1552259334078074932",
 
   // Separate role for Staff Application tickets.
-  staffTestTicketRoleId: "PUT_STAFF_TEST_ROLE_ID_HERE",
+  staffTestTicketRoleId: "1552259662903115896",
 
   // Ticket close logs + transcripts.
-  ticketLogsChannelId: "PUT_TICKET_LOGS_CHANNEL_ID_HERE",
+  ticketLogsChannelId: "1552259782533054504",
 
   // =====================
   // WELCOME
@@ -71,7 +72,7 @@ module.exports = {
   // Leave "" and the bot will try:
   // welcome / welcomes / ברוכים-הבאים
   // then the server System Channel.
-  welcomeChannelId: "",
+  welcomeChannelId: "1552240808172986429",
 
   // =====================
   // XP
@@ -111,7 +112,7 @@ module.exports = {
       emoji: "💙",
       price: 2500,
       roleId:
-        "PUT_SUPPORTER_ROLE_ID_HERE"
+        "1552260029418307585"
     },
     {
       key: "elite",
@@ -119,7 +120,7 @@ module.exports = {
       emoji: "💎",
       price: 5000,
       roleId:
-        "PUT_ELITE_ROLE_ID_HERE"
+        "1552260177896407040"
     },
     {
       key: "legend",
@@ -127,7 +128,7 @@ module.exports = {
       emoji: "👑",
       price: 10000,
       roleId:
-        "PUT_LEGEND_ROLE_ID_HERE"
+        "1552260304686162031"
     }
   ]
 };
